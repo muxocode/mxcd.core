ebff6b96a4fe50a393a0d195f70353c3cd6929cf
Miguel Angel del Campo Morales <miguel.campo@altran.com>
2019-12-18 18:28:23 +0100

    CQRS

---
+++ src/cqrs/ICommander.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mxcd.core.cqrs
+{
+    /// <summary>
+    /// Commands for entity
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface ICommander<T>
+    {
+        /// <summary>
+        /// Add an item
+        /// </summary>
+        /// <param name="item">item to add</param>
+        Task Add(T item);
+        /// <summary>
+        /// Update an entity
+        /// </summary>
+        /// <returns></returns>
+        Task Update(T obj);
+        /// <summary>
+        /// Update by expression
+        /// </summary>
+        /// <typeparam name="TUpdate">Object type</typeparam>
+        /// <param name="expression">expression</param>
+        /// <param name="objToUpdate">data to update</param>
+        /// <returns></returns>
+        Task Update<TUpdate>(Expression<Func<T, bool>> expression, TUpdate objToUpdate);
+        /// <summary>
+        /// Remove entities
+        /// </summary>
+        /// <param name="expression">Expresion</param>
+        /// <returns></returns>
+        Task Delete(Expression<Func<T, bool>> expression);
+        /// <summary>
+        /// Remove an entity
+        /// </summary>
+        /// <param name="key">Unique key</param>
+        /// <returns></returns>
+        Task Delete<TKey>(TKey key);
+    }
+}
---
+++ src/cqrs/IQuerier.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mxcd.core.cqrs
+{
+    /// <summary>
+    /// Querirer
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface IQuerier<T>
+    {
+        /// <summary>
+        /// Gets a collection of T
+        /// </summary>
+        /// <param name="expression">filter</param>
+        /// <returns></returns>
+        Task<IEnumerable<T>> Get(Expression<Func<T>> expression = null);
+        /// <summary>
+        /// Gets a entity
+        /// </summary>
+        /// <typeparam name="TKey">Type of key</typeparam>
+        /// <param name="key">Unique key</param>
+        /// <returns></returns>
+        Task<T> Get<TKey>(TKey key);
+    }
+}
