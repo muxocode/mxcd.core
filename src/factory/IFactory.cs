4742191f93856e60c0afbe6f948087916accaed4
Miguel Angel del Campo Morales <miguel.campo@altran.com>
2019-12-27 16:53:13 +0100

    Add IFactory

---
+++ src/factory/IFactory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace mxcd.core.factory
+{
+    /// <summary>
+    /// Factory pattern
+    /// </summary>
+    public interface IFactory<in TIn,out TOut>
+    {
+        /// <summary>
+        /// Creates an entity of <typeparamref name="TOut"/>
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        TOut Create(TIn obj);
+    }
+}
