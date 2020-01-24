68013e2915a53e5b7e9a263d5413e306efd660f3
Miguel Angel del Campo Morales <miguel.campo@altran.com>
2020-01-24 14:42:48 +0100

    Se añade IErrorHadler

---
+++ src/services/IErrorHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace mxcd.core.services
+{
+    /// <summary>
+    /// Error handler
+    /// </summary>
+    public interface IErrorHandler
+    {
+        /// <summary>
+        /// Trace an error
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="exception"></param>
+        /// <returns></returns>
+        Task Trace(string message, Exception exception=null);
+        /// <summary>
+        /// Trace an error
+        /// </summary>
+        /// <param name="exception"></param>
+        /// <returns></returns>
+        Task Trace(Exception exception);
+
+    }
+}
